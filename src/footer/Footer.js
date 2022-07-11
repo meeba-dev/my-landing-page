44f4d7702d84113b7940afdb92264f5d202d9f54
meeba-dev <mikhailbaum@icloud.com>
2022-07-11 23:29:02 +0400

    init: start react project

---
+++ src/footer/Footer.js
@@ -0,0 +1,34 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer>
+            <ul class="footer-container">
+                <li>
+                    <ul class="footer-socials" id="contacts">
+                        <li>
+                            <a href="https://www.linkedin.com/in/mikhail-baum-2a4391126">
+                                <img src="./img/social/linkedin.png" alt="linkedin"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/mikhaelbaum">
+                                <img src="./img/social/twitter.png" alt="twitter"/>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://t.me/mikhaelbaum">
+                                <img src="./img/social/telegram.png" alt="telegram"/>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <section class="footer-copyrights">
+                <p>&#60; &#62; Made by Mikhail Baum</p>
+            </section>
+        </footer>
+    );
+}
+
+export default Footer;
