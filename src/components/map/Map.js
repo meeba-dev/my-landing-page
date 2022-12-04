15b0f90e18b6afb65531d6f34f8b1fe043dc9b87
meeba-dev <mikhailbaum@icloud.com>
2022-12-05 00:54:11 +0400

    fix: change location to Samara

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -1,11 +1,11 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import './Map.css';
 
-const position = [52.24, 21.02];
+const position = [53.19861, 50.11401];
 
 function Map() {
     return(
-        <MapContainer center={position} zoom={9} scrollWheelZoom={false}>
+        <MapContainer center={position} zoom={10} scrollWheelZoom={false}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
